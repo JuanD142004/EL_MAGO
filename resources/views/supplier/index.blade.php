0f9fd70b3ba3888b7e07fe8af4b8a07faafebe51
JuanD142004 <140752749+JuanD142004@users.noreply.github.com>
2024-04-29 14:57:09 -0500

    Update index.blade.php

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -16,7 +16,17 @@
                          <div class="btn-group">
                             <input type="text" name="busqueda" class="from-control my-2">
                             <input type="submit" value="Buscar" class="btn btn-primary my-2" >
+                            
                          </div>
+                         <div>
+                            @if (!empty($busqueda))
+                                        <div>
+                                            <a href="{{ route('supplier.index') }}" class="btn btn-primary btn-sm">
+                                                {{ __('Volver al índice') }}
+                                            </a>
+                                        </div>
+                                    @endif
+                          </div>
                      </form>
                   </div>
                     <div class="card-header">
