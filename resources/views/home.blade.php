d8d421d6879e51d54242166c63c52d9aee525b6c
BrayanPinilla81233 <bpinilla028@gmail.com>
2024-05-21 13:48:25 -0500

    Dasboard/Accesibilidad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,146 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<br>
+<link rel="stylesheet" href="{{asset('css/dashboard/sb-admin-2.min.css')}}">
+<link rel="stylesheet" href="{{asset('css/dashboard/sb-admin-2.css')}}">
+<title>Inicio</title>
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-sm-12">
             <div class="card">
-                <div class="card-header">{{ __('Holas') }}</div>
+                <div class="card-header">
+                    <h3>{{ __('Panel Administrativo') }}</h3>
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-danger shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                                Proveedor con más compras</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fa-solid fa-user-group fa-2xl"></i>                                        
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-success shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                Total de Ventas Hoy</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fa-solid fa-sack-dollar fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Ventas Realizadas
+                                            </div>
+                                            <div class="row no-gutters align-items-center">
+                                                <div class="col-auto">
+                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="progress progress-sm mr-2">
+                                                        <div class="progress-bar bg-info" role="progressbar"
+                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                                                            aria-valuemax="100"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-warning shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Compras Realizadas</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fa-solid fa-money-check-dollar fa-2xl"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3>Compras Proveedores</h3>
+                            <table class="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                    </tr>
+                                    
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>Ventas Realizadas</h3>
+                            <table class="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                        <th>hola</th>
+                                    </tr>
+                                    
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                        <td>hola</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,19 +7,18 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Distribuciones El Mago</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-    <!-- Custom CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <script src="{{asset('js/tootip.js')}}" defer></script>
     <style>
-        * {
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
+        *{
+            /* font-family: 'arial', cursive; */
         }
     </style>
     <!-- Scripts -->
@@ -28,7 +27,7 @@
 <body>
 
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container d-flex align-items-center">
                 <a class="navbar-brand" href="{{ url('/') }}" style="margin-left: 20px;">
                     <img src="{{ asset('logo-removebg-preview.png') }}" alt="Logo" style="max-width: 80px; margin-right: 10px;">
@@ -112,27 +111,125 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
+        <nav class="navbar navbar-expand-lg navbar-light" style=" background-color: rgb(2, 35, 58)">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo" style="max-width: 70px; margin: 0% 1%">
+            <a class="navbar-brand text-white" href="{{route('home')}}" style="font-family: 'ALGERIAN', cursive;  font-size: 25px;">Distribuciones EL Mago</a>
+            <button class="navbar-toggler" style="background-color: rgb(172, 172, 172); margin-right: 3%" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item active">
+                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
+                            </li>
+                        @endif
 
-        <main class="py-4">
+                        @if (Route::has('register'))
+                            <li class="nav-item active">
+                                <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Registro') }}</a>
+                            </li>
+                        @endif
+                    @else
+                    <li class="nav-item active">
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" role="button" 
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" 
+                            v-pre ><i class="fa-solid fa-user"></i> {{ __('Personas') }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" text-dark" href="{{ route('supplier.index') }}">{{ __('Proveedores') }}</a>
+                            <a class="dropdown-item"text-dark" href="{{ route('employee.index') }}">{{ __('Empleados') }}</a>
+                            <a class="dropdown-item" text-dark" href="{{ route('user.index') }}">{{ __('Usuarios') }}</a>
+                            <a class="dropdown-item" text-dark" href="{{route('customer.index')}}">{{__('Clientes')}}</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('product.index') }}">{{ __('Productos') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('truck_type.index') }}">{{ __('Tipo de Camion') }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{route('load.index')}}">{{__('Carga')}}</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" role="button" 
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" 
+                            v-pre > {{ __('Rutas') }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" text-dark" href="{{ route('route.index') }}">{{ __('Generar Rutas') }}</a>
+                            <a class="dropdown-item" text-dark" href="{{ route('departament.index') }}">{{ __('Departamentos') }}</a>
+                            <a class="dropdown-item"text-dark"  href="{{ route('municipality.index') }}">{{ __('Municipios') }}</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{route('load.index')}}">{{__('Ventas')}}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{route('load.index')}}">{{__('Compras')}}</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" 
+                            href="#" role="button" data-bs-toggle="dropdown" 
+                            aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item " href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                {{ __('Cerrar Sesion') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+                        </div>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
+        </nav>
+        <main>
             @yield('content')
         </main>
+        {{-- <div class="container"> --}}
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-2 px-4 my-2 border-top">
+              <div class="col-md-4 d-flex align-items-center">
+                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                    <img src="{{asset('img/logo.png')}}" alt="Logo" style="max-width: 30px;margin: 0% 4%">
+                </a>
+                <span class="text-muted">&copy; 2024 Distribuciones el Mago, Diseñado por <b>Developer Programer</b></span>
+              </div>
+          
+              <ul class="nav col-md-4 justify-content-end list-unstyled d-flex ">
+                <li class="ms-3"><a class="text-muted" tooltip="tooltip" title="Twitter" href="#"><i class="fa-brands fa-x-twitter fa-xl"></i></a></li>
+                <li class="ms-3"><a class="text-muted" tooltip="tooltip" title="Instagram" href="#"><i class="fa-brands fa-instagram fa-xl"></i></a></li>
+                <li class="ms-3"><a class="text-muted" tooltip="tooltip" title="facebook" href="#"><i class="fa-brands fa-facebook fa-xl"></i></a></li>
+              </ul>
+            </footer>
+        {{-- </div> --}}
     </div>
+    @yield('script')
+    <script src="https://cdn.userway.org/widget.js" data-account="m6oj1qPLRj"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- Bootstrap Bundle with Popper -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Script para alternar la visibilidad de la contraseña -->
     <script>
     document.addEventListener('DOMContentLoaded', function () {
         const passwordField = document.querySelector('#password');
         const togglePassword = document.querySelector('.toggle-password');
-
         togglePassword.addEventListener('click', function () {
             const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
             passwordField.setAttribute('type', type);
         });
     });
     </script>
-    @yield('script')
 </body>
 </html>
