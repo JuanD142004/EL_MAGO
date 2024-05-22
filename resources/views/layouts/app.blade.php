ca9ecfd83a6ad04ffa2db1a9edb8634ae97f5016
Juan Andres Preciado <140751775+Juancardoz@users.noreply.github.com>
2024-05-22 15:06:41 -0500

    Cambios inncesarios
    Juan Ventas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,91 +44,7 @@
 <body>
 
     <div id="app">
-        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container d-flex align-items-center">
-                <a class="navbar-brand" href="{{ url('/') }}" style="margin-left: 20px;">
-                    <img src="{{ asset('logo-removebg-preview.png') }}" alt="Logo" style="max-width: 80px; margin-right: 10px;">
-                    <h1 style="color: black; font-family: 'ALGERIAN', cursive; font-size: 25px;margin-left: 90px;margin-top: -70px;">DISTRIBUCIONES EL MAGO</h1>
-                </a>
-                
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
-                                </li>
-                            @endif
-                        @else
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('user.index') }}">{{ __('Usuarios') }}</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('supplier.index') }}">{{ __('Proveedores') }}</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('employee.index') }}">{{ __('Empleados') }}</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('product.index') }}">{{ __('Productos') }}</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('truck_type.index') }}">{{ __('Tipo de Camion') }}</a>
-                                </li>
-                                <li class="nav-item"> 
-                                     <a class="nav-link" href="{{route('customer.index')}}">{{__('Clientes')}}</a>
-                                </li>
-                                <li class="nav-item"> 
-                                    <a class="nav-link" href="{{route('load.index')}}">{{__('Carga')}}</a>
-                                </li>
-                                 <li class="nav-item">
-                                  <a class="nav-link" href="{{ route('route.index') }}">{{ __('Rutas') }}</a>
-                                </li>
-                                <li class="nav-item">
-                                 <a class="nav-link" href="{{ route('departament.index') }}">{{ __('Departamentos') }}</a>
-                                </li>
-                                <li class="nav-item">
-                                 <a class="nav-link" href="{{ route('municipality.index') }}">{{ __('Municipios') }}</a>
-                                </li>
-
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Cerrar Sesion') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav> --}}
+        
         <nav class="navbar navbar-expand-lg navbar-light" style=" background-color: rgb(2, 35, 58)">
             <img src="{{ asset('img/logo.png') }}" alt="Logo" style="max-width: 70px; margin: 0% 1%">
             <a class="navbar-brand text-white" href="{{route('home')}}" style="font-family: 'ALGERIAN', cursive;  font-size: 25px;">Distribuciones EL Mago</a>
@@ -183,9 +99,6 @@
                             <a class="dropdown-item" text-dark" href="{{ route('departament.index') }}">{{ __('Departamentos') }}</a>
                             <a class="dropdown-item"text-dark"  href="{{ route('municipality.index') }}">{{ __('Municipios') }}</a>
                         </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{route('purchase.index')}}">{{__('Compras')}}</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white" href="{{route('sale.index')}}">{{__('Ventas')}}</a>
