a8bfadc9f09b05799d2c2b905688acbdb7bd705c
JuanD142004 <140752749+JuanD142004@users.noreply.github.com>
2024-06-03 23:23:10 -0500

    traduccion
    arregle la traducion de login del correo que no este en la base de datos

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Validation\ValidationException;
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
@@ -19,4 +19,33 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string|exists:users,email',
+            'password' => 'required|string',
+        ], [
+            'email.exists' => 'El correo electrónico no se encuentra registrado en la base de datos.',
+        ]);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $errors = [$this->username() => trans('auth.failed')];
+
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            $errors['email'] = trans('auth.throttle', ['seconds' => $this->limiter()->availableIn(
+                $this->throttleKey($request)
+            )]);
+        } else {
+            if (!$this->guard()->validate($this->credentials($request))) {
+                $errors = ['password' => 'La contraseña proporcionada es incorrecta.'];
+            }
+        }
+
+        throw ValidationException::withMessages($errors);
+    }
 }
+    
+
